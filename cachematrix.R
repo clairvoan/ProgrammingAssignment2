d25bf24a13778a00d4a4c00e18b4aec0d88118bb
clairvoan <nyu2@ualberta.ca>
2015-10-25 14:50:52 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+mainv <- NULL
+scea <- function(y){
+  x <<- y
+  mainv <<- NULL
+}
+sceb <- function() x
+sceainv <- function(invcal) mainv <<- invcal
+scebinv <- function() mainv
+list(scea = scea, 
+     sceb = sceb, 
+     sceainv = sceainv,
+     scebinv = scebinv)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+mainv <- x$scebinv()
+  if (!is.null(mainv)) {
+    message("data catching")
+    return (mainv)
+  }
+   temp <- x$sceb()
+   mainv <- solve(temp, ...)
+   x$sceainv(mainv)
+   mainv
 }
